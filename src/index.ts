55eb3d0891b48bde686afe28ea56bed80de516dd
David Fernandes de Oliveira <dbfernandes@gmail.com>
2025-09-01 06:37:50 -0400

    Aula 29/08/2025

---
+++ src/index.ts
@@ -0,0 +1,15 @@
+import express from "express"
+import dotenv from "dotenv"
+
+dotenv.config()
+
+const app = express()
+const PORT = process.env.PORT ?? 7788
+
+app.get("/", (req, res) => {
+    res.send("Olá 2!!")
+})
+
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}.`)
+})
