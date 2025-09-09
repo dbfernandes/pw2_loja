2aca4511a32f499ac278eaf42dd066a8b38e9a56
David Fernandes de Oliveira <dbfernandes@gmail.com>
2025-09-09 05:48:52 -0400

    Ajustes gerais

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,15 +1,15 @@
-import express from "express"
-import dotenv from "dotenv"
+import express from "express";
+import dotenv from "dotenv";
 
-dotenv.config()
+dotenv.config();
 
-const app = express()
-const PORT = process.env.PORT ?? 7788
+const app = express();
+const PORT = process.env.PORT ?? 7788;
 
 app.get("/", (req, res) => {
-    res.send("Olá 20!!")
-})
+  res.send("Olá, bem-vindo(a) ao curso de PW2!");
+});
 
 app.listen(PORT, () => {
-    console.log(`App running on port ${PORT}.`)
-})
+  console.log(`App running on port ${PORT}.`);
+});
